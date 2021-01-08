94a704b2959337204fa43098aa5b02560060e106
Collin Redding <TempestTTU@gmail.com>
2021-01-07 21:28:25 -0600

    Update module/class namespace style
    This updates to not opening all of the namespace modules/classes
    when defining sub-modules or sub-classes. This is part of our
    latest conventions.

--- lib/much-slug/activerecord.rb
+++ lib/much-slug/activerecord.rb
@@ -3,61 +3,61 @@
 require "much-mixin"
 require "much-slug"
 
-module MuchSlug
-  module ActiveRecord
-    include MuchMixin
+module MuchSlug; end
 
-    mixin_class_methods do
-      def much_slug_has_slug_registry
-        @much_slug_has_slug_registry ||= MuchSlug::HasSlugRegistry.new
-      end
+module MuchSlug::ActiveRecord
+  include MuchMixin
 
-      def has_slug(
-            source:,
-            attribute:              nil,
-            preprocessor:           nil,
-            separator:              nil,
-            allow_underscores:      nil,
-            skip_unique_validation: false,
-            unique_scope:           nil)
-        registered_attribute =
-          much_slug_has_slug_registry.register(
-            attribute:         attribute,
-            source:            source,
-            preprocessor:      preprocessor,
-            separator:         separator,
-            allow_underscores: allow_underscores,
-          )
-
-        # since the slug isn't written until an after callback we can't always
-        # validate presence of it
-        validates_presence_of(registered_attribute, on: :update)
-
-        unless skip_unique_validation
-          validates_uniqueness_of(
-            registered_attribute,
-            case_sensitive: true,
-            scope:          unique_scope,
-            allow_nil:      true,
-            allow_blank:    true
-          )
-        end
-
-        after_create :much_slug_has_slug_update_slug_values
-        after_update :much_slug_has_slug_update_slug_values
-
-        registered_attribute
-      end
+  mixin_class_methods do
+    def much_slug_has_slug_registry
+      @much_slug_has_slug_registry ||= MuchSlug::HasSlugRegistry.new
     end
 
-    mixin_instance_methods do
-      private
+    def has_slug(
+          source:,
+          attribute:              nil,
+          preprocessor:           nil,
+          separator:              nil,
+          allow_underscores:      nil,
+          skip_unique_validation: false,
+          unique_scope:           nil)
+      registered_attribute =
+        much_slug_has_slug_registry.register(
+          attribute:         attribute,
+          source:            source,
+          preprocessor:      preprocessor,
+          separator:         separator,
+          allow_underscores: allow_underscores,
+        )
 
-      def much_slug_has_slug_update_slug_values
-        MuchSlug.has_slug_changed_slug_values(self) do |attribute, slug_value|
-          public_send("#{attribute}=", slug_value)
-          update_column(attribute, slug_value)
-        end
+      # since the slug isn't written until an after callback we can't always
+      # validate presence of it
+      validates_presence_of(registered_attribute, on: :update)
+
+      unless skip_unique_validation
+        validates_uniqueness_of(
+          registered_attribute,
+          case_sensitive: true,
+          scope:          unique_scope,
+          allow_nil:      true,
+          allow_blank:    true
+        )
+      end
+
+      after_create :much_slug_has_slug_update_slug_values
+      after_update :much_slug_has_slug_update_slug_values
+
+      registered_attribute
+    end
+  end
+
+  mixin_instance_methods do
+    private
+
+    def much_slug_has_slug_update_slug_values
+      MuchSlug.has_slug_changed_slug_values(self) do |attribute, slug_value|
+        public_send("#{attribute}=", slug_value)
+        update_column(attribute, slug_value)
       end
     end
   end
--- lib/much-slug/has_slug_registry.rb
+++ lib/much-slug/has_slug_registry.rb
@@ -2,44 +2,44 @@
 
 require "much-slug"
 
-module MuchSlug
-  class HasSlugRegistry < ::Hash
-    def initialize
-      super{ |h, k| h[k] = Entry.new }
-    end
+module MuchSlug; end
 
-    def register(
-          attribute:,
-          source:,
-          preprocessor:,
-          separator:,
-          allow_underscores:)
-      attribute         = (attribute || MuchSlug.default_attribute).to_s
-      source_proc       = source.to_proc
-      preprocessor_proc = (preprocessor || MuchSlug.default_preprocessor).to_proc
-      separator         = separator || MuchSlug.default_separator
-      allow_underscores =
-        if allow_underscores.nil?
-          MuchSlug.default_allow_underscores
-        else
-          !!allow_underscores
-        end
-
-      entry = self[attribute]
-      entry.source_proc       = source_proc
-      entry.preprocessor_proc = preprocessor_proc
-      entry.separator         = separator
-      entry.allow_underscores = allow_underscores
-
-      attribute
-    end
-
-    Entry =
-      Struct.new(
-        :source_proc,
-        :preprocessor_proc,
-        :separator,
-        :allow_underscores
-      )
+class MuchSlug::HasSlugRegistry < ::Hash
+  def initialize
+    super{ |h, k| h[k] = Entry.new }
   end
+
+  def register(
+        attribute:,
+        source:,
+        preprocessor:,
+        separator:,
+        allow_underscores:)
+    attribute         = (attribute || MuchSlug.default_attribute).to_s
+    source_proc       = source.to_proc
+    preprocessor_proc = (preprocessor || MuchSlug.default_preprocessor).to_proc
+    separator         = separator || MuchSlug.default_separator
+    allow_underscores =
+      if allow_underscores.nil?
+        MuchSlug.default_allow_underscores
+      else
+        !!allow_underscores
+      end
+
+    entry = self[attribute]
+    entry.source_proc       = source_proc
+    entry.preprocessor_proc = preprocessor_proc
+    entry.separator         = separator
+    entry.allow_underscores = allow_underscores
+
+    attribute
+  end
+
+  Entry =
+    Struct.new(
+      :source_proc,
+      :preprocessor_proc,
+      :separator,
+      :allow_underscores
+    )
 end
